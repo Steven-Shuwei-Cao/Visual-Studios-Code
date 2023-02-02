532214607713dd156b78a80302f53e192cb1891d
Steven-Shuwei-Cao <Steven.shuwei.cao@gmail.com>
2023-02-02 16:45:14 -0500

    added comments

--- Practices/FrogRiverOne/frogRiverOne.py
+++ Practices/FrogRiverOne/frogRiverOne.py
@@ -12,7 +12,7 @@ def solution(X, A):
     steps = set()
     time = -1
     for i in range (0, len(A)):
-        if A[i] not in steps and A[i] <= X:
+        if A[i] not in steps and A[i] <= X: #keep track of each steps' first appearance
             time = i
             steps.add(A[i])
     if len(steps) == X:
