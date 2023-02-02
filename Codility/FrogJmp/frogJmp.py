05a7e6fbbc75b8fb06cb28c97e0e185d6ab3251b
Steven-Shuwei-Cao <Steven.shuwei.cao@gmail.com>
2023-02-02 11:40:22 -0500

    another one

---
+++ Codility/FrogJmp/frogJmp.py
@@ -0,0 +1,28 @@
+#==========================================
+# Title:  Codility FrogJmp
+# Author: Steven Cao
+# Date:   2 Feb 2023
+# Score:  100/100
+# Errors: Not an error, but need to int(<float>) before return due to the expected return type
+#==========================================
+
+import unittest
+
+def solution(X, Y, D):
+    dist = Y - X
+    if dist % D != 0:
+        return int(dist/D) + 1
+    return int(dist/D)
+
+
+test = (10,85,30,3), (125,1111,33,30)
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_split(self):
+        for ele in test:
+            self.assertEqual(solution(ele[0], ele[1], ele[2]), ele[3])
+        #check the result against the expected number of gaps
+
+if __name__ == '__main__':
+    unittest.main()
